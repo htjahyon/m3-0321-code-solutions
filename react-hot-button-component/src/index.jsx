66595a1e503ae38581c6fb521d34a44923d67912
Hanli Tjahyono <hansmans326113@aol.com>
2021-03-19 23:21:22 -0700

    removed first setState

--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -12,7 +12,6 @@ class HotButton extends React.Component {
   handleClick() {
     this.clicks++;
     const clicks = this.clicks;
-    this.setState({ backgroundColor: 'gray' });
     if (clicks >= 18) { this.setState({ backgroundColor: 'white' }); } else if (clicks >= 15) { this.setState({ backgroundColor: 'yellow' }); } else if (clicks >= 12) { this.setState({ backgroundColor: 'rgb(255,212,128)' }); } else if (clicks >= 9) { this.setState({ backgroundColor: 'pink' }); } else if (clicks >= 6) { this.setState({ backgroundColor: 'rgb(102,0,204)' }); } else if (clicks >= 3) { this.setState({ backgroundColor: 'purple' }); }
   }
 
